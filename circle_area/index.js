d677989268c4687db509ee03414d3985c5bfea60
Ives Samuel <48871044+Samuelves@users.noreply.github.com>
2021-02-21 07:47:57 -0800

    Update index.js

--- circle_area/index.js
+++ circle_area/index.js
@@ -2,6 +2,6 @@ var input = require('fs').readFileSync('./stdin', 'utf8');
 var lines = input.split('\n');
 const pi = 3.14159;
 
-const calc = pi * (lines[0] * lines[0])
+const ca = pi * (lines[0] * lines[0])
 
-console.log(`A=${calc.toFixed(4)}\n`);
+console.log(`A=${ca.toFixed(4)}\n`);
